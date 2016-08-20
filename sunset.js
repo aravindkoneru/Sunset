16796fd5d213625a7882afd312674170e5946884
Aravind Koneru <aravind.b.koneru@gmail.com>
2016-08-20 12:06:09 -0400

    fixed timezone issue

--- sunset.js
+++ sunset.js
@@ -2,12 +2,13 @@ let http = require('http');
 let Q = require('q');
 let dateFormat = require('dateformat');
 let timezoner = require('timezoner');
+
 let google_api_key = 'AIzaSyDTge0Q1I6U2a3ARZZma3g68ly5ULi7GJI';
 let api_key_expr = '&APPID=f76bc113223d802d5568ee1f9fc5818a';
 const current_colleges = [
 	{name: "Rutgers", id: 5101717, coord:{lon:-74.45182,lat:40.486221}},
 	{name: "UIUC", id: 4914570, coord:{lon:-88.207268, lat:40.110592}},
-	{name: "UC Berkley", id: 5413519, coord:{lon:-105.026093, lat:39.804428}},
+	{name: "UC Berkley", id: 5327684, coord:{lon:-122.272751, lat:37.87159}},
 	{name: "U Pittsburgh", id: 5206379, coord:{lon:-79.995888, lat:40.44062}},
 	{name: "U Maryland", id: 4351977, coord:{lon:-76.93692, lat:38.980671}},
 	{name: "Bentley University", id: 4954380, coord:{lon:-71.235611, lat:42.376492}},
@@ -29,43 +30,17 @@ const current_colleges = [
 	{name: "Vanderbuilt", id: 4644585, coord:{lon:-86.784439, lat:36.16589}}
 ];
 
-function getCityData(city_id){
-	let deffered = Q.defer();
-
-	var options = {
-    	host: 'api.openweathermap.org',
-    	path: '/data/2.5/weather?id=' + city_id + api_key_expr,
-  	};
-
-  callback = function(response) {
-    var str = '';
-
-    response.on('data', function(chunk) {
-      str += chunk;
-    });
-
-    response.on('end', function() {
-      var parsedEventData = JSON.parse(str);
-      deffered.resolve(parsedEventData.sys.sunset);
-    });
-  };
-
-  http.request(options, callback).end();
-  return deffered.promise;
-}
-
 function getCollegeData(colleges){
 	let deffered = Q.defer();
 	let counter = 0;
 	for(let x = 0; x < colleges.length; x++){
-		let promise = getCityData(colleges[x].id);
+		let promise = getCityData(colleges[x].id, x);
 		promise.then(function(time){
-			console.log(time);
-			colleges[x].sunset = time;
-			let pos = colleges[x].coord;
-			return getTimeZoneOffset(pos);
+			colleges[time.index].sunset = time.sunset;
+			let pos = colleges[time.index].coord;
+			return getTimeZoneOffset(pos, time.index);
 		}).then(function(offset){
-			colleges[x].offset = offset; 
+			colleges[offset.index].offset = offset.offset; 
 		}).then(function(){
 			if(counter == colleges.length-1){
 				deffered.resolve(colleges);
@@ -77,7 +52,36 @@ function getCollegeData(colleges){
 	return deffered.promise;
 }
 
-function getTimeZoneOffset(pos){
+function getCityData(city_id, index){
+	let deffered = Q.defer();
+
+	var options = {
+    	host: 'api.openweathermap.org',
+    	path: '/data/2.5/weather?id=' + city_id + api_key_expr,
+  	};
+
+  callback = function(response) {
+    let str = '';
+
+    response.on('data', function(chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function() {
+      let parsedEventData = JSON.parse(str);
+      let return_obj = {
+      	sunset: parsedEventData.sys.sunset,
+      	index: index
+      };
+      deffered.resolve(return_obj);
+    });
+  };
+
+  http.request(options, callback).end();
+  return deffered.promise;
+}
+
+function getTimeZoneOffset(pos, index){
 	let deffered = Q.defer();
 
 	timezoner.getTimeZone(pos.lat, pos.lon, 
@@ -85,7 +89,10 @@ function getTimeZoneOffset(pos){
 			if(err){
 				console.log(err);
 			} else{
-				deffered.resolve((data.rawOffset + data.dstOffset)/3600);
+				deffered.resolve({
+					offset: (data.rawOffset + data.dstOffset)/3600,
+					index: index
+				});
 			}
 		},
 		{language: 'en', key: google_api_key}
@@ -97,21 +104,37 @@ function getTimeZoneOffset(pos){
 function composeMessage(colleges){
 	let now = new Date(Date.now());
 	let message = "Sunsets for " +  dateFormat(now, "dddd, mmmm dS, yyyy \n");
+
 	for(let x = 0; x < colleges.length; x++){
 		let sunset = new Date(colleges[x].sunset * 1000);
-		sunset.setHours(sunset.getHours() + colleges[x].offset + 4);
-		let curr_line = colleges[x].name + ": " + dateFormat(sunset, "hh:mm tt") + "\n";
+		let offset = colleges[x].offset;
+		sunset = new Date(sunset.getFullYear(), sunset.getMonth(), sunset.getDate(), sunset.getUTCHours() + offset, sunset.getMinutes());
+		let curr_line = colleges[x].name + ": " + format_time(sunset) + "\n";
 		message += curr_line;
 	}
 	return message;
 }
 
+function format_time(date_obj) {
+  // formats a javascript Date object into a 12h AM/PM time string
+  var hour = date_obj.getHours();
+  var minute = date_obj.getMinutes();
+  var amPM = (hour > 11) ? "pm" : "am";
+  if(hour > 12) {
+    hour -= 12;
+  } else if(hour == 0) {
+    hour = "12";
+  }
+  if(minute < 10) {
+    minute = "0" + minute;
+  }
+  return hour + ":" + minute + amPM;
+}
 
 function getSunsets(colleges){
 	let promise = getCollegeData(colleges);
 	promise.then(function(college_sunsets){
 		console.log(composeMessage(college_sunsets))
-		//console.log(colleges);
 	}).done();
 }
 
