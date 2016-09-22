b82c05103336c11745a1c7aabfc08ed3d408c27e
Aravind Koneru <aravind.b.koneru@gmail.com>
2016-09-22 08:56:14 -0400

    started working on memes

--- scrape.py
+++ scrape.py
@@ -1,14 +1,33 @@
 import praw
 import codecs
+import urllib
 
 
 user_agent = ("CopyPasta 1.0")
-
 request = praw.Reddit(user_agent = user_agent)
-copypastas = request.get_subreddit("copypasta").get_top(limit = 1)
-copypasta = next(copypastas)
 
-copypasta_file = codecs.open('pasta_of_the_day.txt', 'w', 'utf-8')
-copypasta_file.write(copypasta.selftext)
+def writeCopyPasta:
+	copypastas = request.get_subreddit("copypasta").get_top(limit = 1)
+	copypasta = next(copypastas)
 
-copypasta_file.close()
+	copypasta_file = codecs.open('pasta_of_the_day.txt', 'w', 'utf-8')
+	copypasta_file.write(copypasta.selftext)
+
+	copypasta_file.close()
+
+def writeDankMeme:
+	dank_memes = request.get_subreddit("BikiniBottomTwitter").get_top(limit=1)
+	meme = next(dank_memes)
+
+	# for meme in dank_memes:
+	# 	print meme.url
+
+	urllib.urlretrieve(meme.url, "00000001.jpg")
+
+	# print(dank_meme)
+
+# direct image link: http://i.imgur.com/azDHl29.png
+# imgur link: http://imgur.com/azDHl29
+# imgur album link: http://imgur.com/a/WDwyW
+
+#general form: http://{i.}imgur.com/{a/}[sometext]{.png}
